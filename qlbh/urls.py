84145ca9679fc7848c3733d801903e86189aad40
tienthanh89 <tienthanh8925@gmail.com>
2025-06-14 18:32:07 +0700

    update views

--- qlbh/urls.py
+++ qlbh/urls.py
@@ -4,10 +4,16 @@ from .views import DoanhThuCuaHangView, NhanVienView, SanPhamView, KhachHangView
 
 urlpatterns = [
     path('index', views.index, name='index'),
-    path('nhanvien/<str:manv>', NhanVienView.as_view(), name='nhanvien'),
+
+    path('nhanvien/<str:manv>', NhanVienView.as_view(), name='nhanvien_manv'),
     path('nhanvien', NhanVienView.as_view(), name='nhanvien'),
-    path('sanpham', SanPhamView.as_view(), name='nhanvien'),
+
+    path('sanpham', SanPhamView.as_view(), name='sanpham'),
+    path('sanpham/<str:masp>', SanPhamView.as_view(), name='sanpham_masp'),
+
     path('khachhang', KhachHangView.as_view(), name='khachhang'),
+    path('khachhang/<str:makh>', KhachHangView.as_view(), name='khachhang_makh'),
+
     path('doanh-so-khach-hang', DoanhSoKhachHangView.as_view(), name='doanh-so-khach-hang'),
     path('doanh-thu/', DoanhThuCuaHangView.as_view(), name='doanh_thu_cua_hang'),
 ]
