81e18375ee288f8ed0a914b8e323d83b36bf4439
tienthanh89 <tienthanh8925@gmail.com>
2025-06-14 18:42:14 +0700

    update views

--- qlbh/views.py
+++ qlbh/views.py
@@ -19,7 +19,7 @@ from .service.tri_gia_max_min import tri_gia_max_min
 def index(request):
     return HttpResponse('Xin chào bạn đã đến app QLBH.')
 
-# --- Lớp NhanVienView ---
+# --- NhanVienView ---
 @method_decorator(csrf_exempt, name='dispatch')
 class NhanVienView(View):
     def get(self, request):
@@ -118,7 +118,7 @@ class NhanVienView(View):
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
 
-# --- Lớp SanPhamView ---
+# --- SanPhamView ---
 @method_decorator(csrf_exempt, name='dispatch')
 class SanPhamView(View):
     def get(self, request):
@@ -215,102 +215,6 @@ class SanPhamView(View):
             return JsonResponse({"error": "Sản phẩm không tồn tại."}, status=404)
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
-
-'Tim sp theo ma'
-
-class TimSanPhamTheoMaView(View):
-    def get(self, request):
-        ma = request.GET.get('masp', '').strip()
-
-        ket_qua = SanPham.objects.filter(masp__iexact=ma)
-        if ket_qua.exists():
-            data = list(ket_qua.values())
-            return JsonResponse({
-                "message": f"Tìm thấy {ket_qua.count()} sản phẩm.",
-                "data": data
-            })
-        else:
-            return JsonResponse({
-                "error": "Không tìm thấy sản phẩm nào."
-            }, status=404)
-
-'Tim sp theo ten'
-
-class TimSanPhamTheoTenView(View):
-    def get(self, request):
-        tensp = request.GET.get('tensp', '').strip()
-
-        ket_qua = SanPham.objects.filter(tensp__icontains=tensp)
-
-        if ket_qua.exists():
-            data = list(ket_qua.values())
-            return JsonResponse({
-                "message": f"Tìm thấy {ket_qua.count()} sản phẩm.",
-                "data": data
-            })
-        else:
-            return JsonResponse({
-                "error": "Không tìm thấy sản phẩm nào."
-            }, status=404)
-
-'Tim sp theo nuoc'
-
-class TimSanPhamTheoNuocSXView(View):
-    def get(self, request):
-        nuoc = request.GET.get('nuocsx', '').strip()
-
-        ket_qua = SanPham.objects.filter(nuocsx__icontains=nuoc)
-
-        if ket_qua.exists():
-            data = list(ket_qua.values())
-            return JsonResponse({
-                "message": f"Tìm thấy {ket_qua.count()} sản phẩm từ {nuoc}.",
-                "data": data
-            })
-        else:
-            return JsonResponse({
-                "error": f"Không tìm thấy sản phẩm nào từ {nuoc}."
-            }, status=404)
-
-'Tim top 3 san pham mua nhieu'
-
-class SanPhamMuaNhieuNhatView(View):
-    def get(self, request):
-        ket_qua = (
-            CTHD.objects.values('masp__masp', 'masp__tensp')
-            .annotate(tong_sl=Sum('sl'))
-            .order_by('-tong_sl')[:3]
-        )
-
-        if ket_qua:
-            return JsonResponse({
-                "message": "Top 3 sản phẩm được mua nhiều nhất",
-                "data": list(ket_qua)
-            })
-        else:
-            return JsonResponse({
-                "error": "Không có dữ liệu mua hàng."
-            }, status=404)
-
-'Tim top 3 san pham mua it'
-
-class SanPhamMuaItNhatView(View):
-    def get(self, request):
-        ket_qua = (
-            CTHD.objects.values('masp__masp', 'masp__tensp')
-            .annotate(tong_sl=Sum('sl'))
-            .order_by('tong_sl')[:3]
-        )
-
-        if ket_qua:
-            return JsonResponse({
-                "message": "Top 3 sản phẩm được mua ít nhất",
-                "data": list(ket_qua)
-            })
-        else:
-            return JsonResponse({
-                "error": "Không có dữ liệu mua hàng."
-            }, status=404)
 
 # --- KhachHangView Class ---
 @method_decorator(csrf_exempt, name='dispatch')
@@ -416,7 +320,49 @@ class KhachHangView(View):
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
 
-# --- HoaDonView Class ---
+# --- SanPhamMuaNhieuNhatView ---
+@method_decorator(csrf_exempt, name='dispatch')
+class SanPhamMuaNhieuNhatView(View):
+    """Tim top 3 san pham mua nhieu"""
+    def get(self, request):
+        ket_qua = (
+            CTHD.objects.values('masp__masp', 'masp__tensp')
+            .annotate(tong_sl=Sum('sl'))
+            .order_by('-tong_sl')[:3]
+        )
+
+        if ket_qua:
+            return JsonResponse({
+                "message": "Top 3 sản phẩm được mua nhiều nhất",
+                "data": list(ket_qua)
+            })
+        else:
+            return JsonResponse({
+                "error": "Không có dữ liệu mua hàng."
+            }, status=404)
+
+# --- SanPhamMuaItNhatView ---
+@method_decorator(csrf_exempt, name='dispatch')
+class SanPhamMuaItNhatView(View):
+    """Tim top 3 san pham mua it"""
+    def get(self, request):
+        ket_qua = (
+            CTHD.objects.values('masp__masp', 'masp__tensp')
+            .annotate(tong_sl=Sum('sl'))
+            .order_by('tong_sl')[:3]
+        )
+
+        if ket_qua:
+            return JsonResponse({
+                "message": "Top 3 sản phẩm được mua ít nhất",
+                "data": list(ket_qua)
+            })
+        else:
+            return JsonResponse({
+                "error": "Không có dữ liệu mua hàng."
+            }, status=404)
+
+# --- DoanhSoKhachHangView ---
 @method_decorator(csrf_exempt, name='dispatch')
 class DoanhSoKhachHangView(View):
     def get(self, request):
@@ -445,7 +391,7 @@ class DoanhSoKhachHangView(View):
         except Exception as e:
             return JsonResponse({"error": f"Đã xảy ra lỗi khi tính doanh số: {str(e)}"}, status=500)
 
-# --- Lớp DoanhThuCuaHangView ---
+# --- DoanhThuCuaHangView ---
 @method_decorator(csrf_exempt, name='dispatch')
 class DoanhThuCuaHangView(View):
     def get(self, request):
