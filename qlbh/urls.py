a1480d2957a8a37bdeacf2ba9a0b54ca4c2e52e3
nguyenly2311 <nguyenphucly2311@gmail.com>
2025-06-16 23:55:13 +0700

    commit

--- qlbh/urls.py
+++ qlbh/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
 from .views import  DoanhThuCuaHangView, NhanVienView
+from .service import views_auth
 
 urlpatterns = [
     path('index', views.index, name='index'),
     path('nhanvien/<str:manv>', NhanVienView.as_view(), name='nhanvien'),
     path('nhanvien', NhanVienView.as_view(), name='nhanvien'),
     path('doanh-thu/', DoanhThuCuaHangView.as_view(), name='doanh_thu_cua_hang'),
+    path('register/', views_auth.register_view),
+    path('login/', views_auth.login_view),
+    path('forgot-password/', views_auth.forgot_password_view),
 ]
